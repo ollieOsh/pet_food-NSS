fad2df48c992e283a4a161f6bceb0c5cac1c0814
ollieOsh <sosinusi@gmail.com>
2017-05-09 16:03:15 -0500

    Doneso. No styling though :)

--- food.js
+++ food.js
@@ -1,13 +1,18 @@
-var petFood = new XMLHttpRequest();
-let dogFood = document.getElementById('dog');
+let dogFood = document.getElementById('dog'),
+	catFood = document.getElementById('cat');
+
+var petFood = new XMLHttpRequest(),
+	petCat = new XMLHttpRequest();
 
 petFood.addEventListener('load', petLoad);
 petFood.addEventListener('error', petFail);
 
+petCat.addEventListener('load', catLoad);
+petCat.addEventListener('error', petFail);
+
 function petLoad(event) {
 	console.log('success');
 	let food = JSON.parse(event.target.responseText);
-	console.log("food.json", food);
 	showFood(food);
 }
 
@@ -17,28 +22,66 @@ function petFail(event) {
 
 function showFood(stuff) {
 	let arr = stuff.dog_brands;
-	console.log(arr);
 
 	arr.forEach(function(obj) {
-		console.log("brand", obj.name);
 		dogFood.innerHTML += `<tr id="${obj.name}">
 								<td>${obj.name}</td>
 							</tr>`;
 		let types = obj.types,
 			brand = document.getElementById(`${obj.name}`);
 
-		console.log(brand);
 		var str = `<td>`;
 
 		types.forEach(function(obj2) {
 			let type = obj2.type,
 				volumes = obj2.volumes;
 			for (prop in volumes){
-				//console.log(type);
 				let volume = volumes[prop].name,
 					price = volumes[prop].price;
 
-				console.log(brand, "type", type, "volume", volume, "price", price);
+				str += `&nbsp; ${type}: ${volume} - ${price}. `;
+			}
+		});
+		str += `</td>`;
+		brand.innerHTML += str;
+	})
+}
+
+function catLoad(event) {
+	console.log('success');
+	let food = JSON.parse(event.target.responseText);
+	showCat(food);
+}
+
+function showCat(stuff) {
+	let arr = stuff.cat_brands;
+
+	arr.forEach(function(obj) {
+		catFood.innerHTML += `<tr id="${obj.name}">
+								<td>${obj.name}</td>
+							</tr>`;
+		let types = obj.types,
+			breeds = obj.breeds,
+			brand = document.getElementById(`${obj.name}`);
+
+		var str = `<td>`;
+
+		breeds.forEach(function(breed) {
+			console.log(breed);
+			str += `${breed}, `;
+		});
+
+		str = str.slice(0, -2);
+		str += `</td>
+				<td>`
+
+		types.forEach(function(obj2) {
+			let type = obj2.type,
+				volumes = obj2.volumes;
+			for (prop in volumes){
+				let volume = volumes[prop].name,
+					price = volumes[prop].price;
+
 				str += `&nbsp; ${type}: ${volume} - ${price}. `;
 			}
 		});
@@ -49,3 +92,6 @@ function showFood(stuff) {
 
 petFood.open('GET','food.json');
 petFood.send();
+
+petCat.open('GET','cat.json');
+petCat.send();
